aa5d7b73c9dad0454e0233f30c8bd944789dee09
rkdcodus <rcd7275@naver.com>
2025-08-29 17:12:30 +0900

    [GSAP-칵테일] Cocktails 페이지 구현, 잎파리 스크롤 모션 구현

--- GSAP/cocktail-website/src/App.jsx
+++ GSAP/cocktail-website/src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,7 +12,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-      <div className='h-dvh bg-black'></div>
+      <Cocktails />
     </main>
   );
 };
