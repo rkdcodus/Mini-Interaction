f9d9bf19c867afe98b0a1ed653a3e9dca2514700
rkdcodus <rcd7275@naver.com>
2025-08-29 15:29:30 +0900

    [GSAP-칵테일] ScrollTrigger 적용 안됨 문제 해결, 잎 모션 구현

--- GSAP/cocktail-website/src/components/Hero.jsx
+++ GSAP/cocktail-website/src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
-import React from "react";
 
 const Hero = () => {
   useGSAP(() => {
@@ -25,6 +24,18 @@ const Hero = () => {
       stagger: 0.06,
       delay: 1,
     });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
   }, []);
 
   return (
