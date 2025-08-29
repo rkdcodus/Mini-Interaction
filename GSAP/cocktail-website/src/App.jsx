14315f858b678bf8b87ed3f5f191b07c7fee007d
rkdcodus <rcd7275@naver.com>
2025-08-29 14:31:08 +0900

    [GSAP - 칵테일] 환경 세팅 및 Navbar, Hero 구현
    navbar 스크롤 시 블러 처리 ( timeline-scrollTrigger, fromTo 사용)
    hero 페이지 랜딩 시 모션 추가 (SplitText, from 사용)

---
+++ GSAP/cocktail-website/src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
