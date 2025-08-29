d4caa882f907b69ebf3eced400928d33fafaf594
rkdcodus <rcd7275@naver.com>
2025-08-29 17:26:11 +0900

    [GSAP-칵테일] 칵테일 모바일 화면일 때 스크롤 범위 조정

--- GSAP/cocktail-website/src/components/Hero.jsx
+++ GSAP/cocktail-website/src/components/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
       .to(".left-leaf", { y: -200 }, 0);
 
     const startValue = isMobile ? "top 50%" : "center 60%";
-    const endValue = isMobile ? "120% top" : "bottom top";
+    const endValue = isMobile ? "160% top" : "bottom top";
 
     let tl = gsap.timeline({
       scrollTrigger: {
